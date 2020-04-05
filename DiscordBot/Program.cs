934f8cdd986ed1ed66a5dfc617a842c75140cc85
Aaro Tuukkanen <artzi@segfault.fi>
2020-04-05 14:27:53 +0300

    Change to .NET Core 3.1

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -10,12 +10,12 @@ namespace DiscordBot
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             new Program().MainAsync().GetAwaiter().GetResult();
         }
 
-        public async Task MainAsync()
+        async Task MainAsync()
         {
             Console.WriteLine("Starting...");
 
