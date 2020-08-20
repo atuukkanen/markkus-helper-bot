acc02e217d832bb5bd2cfcbd0b1854f717e672b9
Aaro Tuukkanen <artzi@segfault.fi>
2020-08-20 15:17:04 +0300

    Read config from environment variables

--- DiscordBot/Config/Config.cs
+++ DiscordBot/Config/Config.cs
@@ -1,19 +1,29 @@
-﻿namespace DiscordBot.Configuration
+﻿using System;
+
+namespace DiscordBot.Configuration
 {
     public class Config
     {
+        private const string EnvironmentClientTokenVariableName = "DiscordClientToken";
+        private const string EnvironmentDarkroomApiTokenName = "DarkroomApiToken";
+
         private static Config _config;
 
         private Config()
         {
         }
 
-        public static Config Instance => _config ?? (_config = ReadConfig());
+        public static Config Instance => _config ??= ReadConfig();
 
         public DiscordConfig Discord { get; set; }
 
         private static Config ReadConfig()
         {
+            var environmentConfig = ReadConfigFromEnvironment();
+            if (environmentConfig != null)
+            {
+                return environmentConfig;
+            }
             var newConfig = new Config
             {
                 Discord = new DiscordConfig
@@ -23,6 +33,25 @@
                 }
             };
             return newConfig;
+        }
+
+        private static Config ReadConfigFromEnvironment()
+        {
+            var t = Environment.GetEnvironmentVariable(EnvironmentClientTokenVariableName);
+            var d = Environment.GetEnvironmentVariable(EnvironmentDarkroomApiTokenName);
+            if (t != null && d != null)
+            {
+                return new Config()
+                {
+                    Discord = new DiscordConfig()
+                    {
+                        ClientToken = t,
+                        DarkroomApiToken = d
+                    }
+                };
+            }
+
+            return null;
         }
 
         public class DiscordConfig
